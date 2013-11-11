c9949ae1f1e10a49dc39d3b07947435e4276046b
Paul Reinheimer <paul@preinheimer.com>
2013-11-11 16:00:50 +0000

    Initial work supporting actually sending requests to the server

---
+++ webroot/submit.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This file needs to:
+ * 1. Pull in the apiconfig.json file
+ * 2. Pull in the API specific json file
+ * 3. Confirm the incoming request contains the necesary data
+ * 4. Construct the request to the desired server
+ * 5. Return the response
+*/
+
