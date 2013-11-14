54fa09c710c135c3c0f77506665f4293f00b010a
Paul Reinheimer <paul@preinheimer.com>
2013-11-14 09:21:04 +0000

    Updating JS with debug code

--- webroot/js/api.js
+++ webroot/js/api.js
@@ -1,7 +1,7 @@
 $(function() { //shorthand document.ready function
     $('.apiform').on('submit', function(e) { //use on if jQuery 1.7+
         e.preventDefault();  //prevent form from submitting
-        var data = $("#login_form :input").serializeArray();
+        var data = $(".apiform :input").serializeArray();
         console.log(data); //use the console for debugging, F12 in Chrome, not alerts
     });
 });
